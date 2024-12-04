c0381512b6c8beb289493f1faf7032a00498e696
Berat Sakipi <berat.sakipi@gmail.com>
2024-12-04 23:59:23 +0100

    Drawing the cards using the deckID to draw two cards from the deckofcardsapi

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const newDeckButton = document.querySelector('#new-deck')
-
+let deckID;
 
 const getDeck = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        deckID = data.deck_id
+        console.log(deckID)
+    })
 }
 
 newDeckButton.addEventListener('click', getDeck)
+
+document.getElementById('draw-cards').addEventListener('click', () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckID}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            data.cards.forEach(card => console.log(card.value + card.suit))
+        })
+})
