73a73e0d5e82cbc1536661e6c2b179df7e7c5a23
Berat Sakipi <berat.sakipi@gmail.com>
2024-12-05 23:19:28 +0100

    Comparing the cards' values and logging to the console the value of the greater on or 'draw' if the values are the same

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-const newDeckButton = document.querySelector('#new-deck')
 let deckID;
+const drawCardsBtn = document.getElementById('draw-cards')
+const newDeckBtn = document.querySelector('#new-deck')
+const cardsContainer = document.querySelector('#cards-container')
 
 const getDeck = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then(res => res.json())
     .then(data => {
         deckID = data.deck_id
-        console.log(deckID)
     })
 }
 
-newDeckButton.addEventListener('click', getDeck)
-
-document.getElementById('draw-cards').addEventListener('click', () => {
+const drawCards = () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckID}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            data.cards.forEach(card => {
-                const cards = document.querySelector('#cards')
-                cards.innerHTML += `
-                    <img src="${card.image}"></img>
-                `
-            })
+            console.log(data.cards)
+            cardsContainer.children[0].innerHTML = `
+                <img src="${data.cards[0].image}"></img>
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src="${data.cards[1].image}"></img>
+            `
+            getTheHighest(data.cards[0], data.cards[1])
         })
-})
+}
+
+const getTheHighest = (card1, card2) => {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+        "10", "JACK", "QUEEN", "KING", "ACE"]
+        const card1Value = valueOptions.indexOf(card1.value)
+        const card2Value = valueOptions.indexOf(card2.value)
+    
+        parseInt(card1Value) > parseInt(card2Value)
+        ? console.log(valueOptions[card1Value])
+        : parseInt(card1Value) < parseInt(card2Value)
+            ? console.log(valueOptions[card2Value])
+            : console.log("draw");
+    
+}
+
+newDeckBtn.addEventListener('click', getDeck)
+drawCardsBtn.addEventListener('click', drawCards)
